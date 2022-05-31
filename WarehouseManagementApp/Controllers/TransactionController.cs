50b890bd95d290b9b58ed43939b52f7bf6b8caa6
artgl42 <artgl.net@gmail.com>
2022-05-31 11:17:35 +0700

    feat: implement TransactionController api functionality (get all, get by id, create, update, delete)

---
+++ WarehouseManagementApp/Controllers/TransactionController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WarehouseManagementApp.Models;
+
+namespace WarehouseManagementApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TransactionController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        public TransactionController(AppDbContext dbContext) => _db = dbContext;
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Transaction>>> GetTransactionaAsync()
+        {
+            return Ok(await _db.Transactions.ToArrayAsync());
+        }
+
+        [HttpGet("{transactionID}")]
+        public async Task<ActionResult<Transaction>> GetTransactionAsync(int transactionID)
+        {
+            var _transaction = await _db.Transactions
+                .FirstOrDefaultAsync(transaction => transaction.ID == transactionID);
+
+            if (_transaction is null)
+            {
+                return NotFound();
+            }
+
+            return new JsonResult(_transaction);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<Transaction>>> CreateTransactionAsync(Transaction transactionToCreate)
+        {
+            if (transactionToCreate is null)
+            {
+                return BadRequest();
+            }
+
+            _db.Transactions.Add(transactionToCreate);
+            await _db.SaveChangesAsync();
+            return Ok(await _db.Transactions.ToArrayAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<IEnumerable<Transaction>>> UpdateTransactionAsync(Transaction transactionToUpdate)
+        {
+            if (transactionToUpdate is null)
+            {
+                return BadRequest();
+            }
+
+            if (!_db.Transactions.Any(transaction => transaction.ID == transactionToUpdate.ID))
+            {
+                return NotFound();
+            }
+
+            _db.Transactions.Update(transactionToUpdate);
+            await _db.SaveChangesAsync();
+            return Ok(await _db.Transactions.ToArrayAsync());
+        }
+
+        [HttpDelete("{transactionID}")]
+        public async Task<ActionResult<IEnumerable<Transaction>>> DeleteProductAsync(int transactionID)
+        {
+            var _transactionToDel = await _db.Transactions.
+                FirstOrDefaultAsync(transaction => transaction.ID == transactionID);
+
+            if (_transactionToDel is null)
+            {
+                return NotFound();
+            }
+
+            _db.Transactions.Remove(_transactionToDel);
+            await _db.SaveChangesAsync();
+            return Ok(await _db.Transactions.ToArrayAsync());
+        }
+    }
+}
--- WarehouseManagementApp/Models/Transaction.cs
+++ WarehouseManagementApp/Models/Transaction.cs
@@ -1,15 +1,24 @@
-﻿namespace WarehouseManagementApp.Models
+﻿using System.Text.Json.Serialization;
+
+namespace WarehouseManagementApp.Models
 {
     public class Transaction
     {
         public int ID { get; set; }
-        public DateTime DateTime { get; set; }
+        public DateTime? DateTime { get; set; }
+
+        [JsonIgnore]
+        public Warehouse? WarehouseFrom { get; set; }
         public int WarehouseFromID { get; set; }
-        public Warehouse WarehouseFrom { get; set; }
+
+        [JsonIgnore]
+        public Warehouse? WarehouseIn { get; set; }
         public int WarehouseInID { get; set; }
-        public Warehouse WarehouseIn { get; set; }
+
+        [JsonIgnore]
+        public Product? Product { get; set; }
         public int ProductID { get; set; }
-        public Product Product { get; set; }
+
         public int Count { get; set; }
     }
 }
