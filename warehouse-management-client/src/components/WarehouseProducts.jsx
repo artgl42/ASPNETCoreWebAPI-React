e3e6ad01f81c6a2054a9d0a9035061a7b5f54032
artgl42 <artgl.net@gmail.com>
2022-06-10 10:48:16 +0700

    fix: WarehouseProducts -> correct format date + button for load table of products

--- warehouse-management-client/src/components/WarehouseProducts.jsx
+++ warehouse-management-client/src/components/WarehouseProducts.jsx
@@ -2,18 +2,17 @@ import React, { useState } from "react";
 import Constants from "../Constants";
 
 export default function WarehouseProducts(props) {
-  const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState(new Date().toISOString().slice(0,10));
   const [products, setProducts] = useState([]);
 
   function handleChange(e) {
     setDate(e.target.value);
-    getProducts();
   }
 
   function getProducts() {
-    const formatedDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+    setProducts([]);
+    const formatedDate = `${new Date(date).getFullYear()}-${new Date(date).getMonth() + 1}-${new Date(date).getDate()}`;
     const url = `${Constants.API_URL_GET_ALL_PRODUCTS_ON_DATE}/${formatedDate}/${props.value}`;
-    //console.log(url);
 
     fetch(url, {
       method: "GET",
@@ -29,7 +28,13 @@ export default function WarehouseProducts(props) {
 
   return (
     <div>
-      <input type="date" defaultValue={date} onChange={handleChange} />
+      <input type="date" value={date} onChange={handleChange} />
+      <button
+        onClick={getProducts}
+        className="btn btn-outline-success btn-sm mx-1 my-1"
+      >
+        Show
+      </button>
       <div className="table-responsive">
         <table className="table table-striped table-bordered table-hover table-sm">
           <thead>
@@ -40,7 +45,7 @@ export default function WarehouseProducts(props) {
           </thead>
           <tbody>
             {products.map((product) => (
-              <tr key={product.id}>
+              <tr key={product.product.id}>
                 <td>{product.product.name}</td>
                 <td>{product.count}</td>
               </tr>
